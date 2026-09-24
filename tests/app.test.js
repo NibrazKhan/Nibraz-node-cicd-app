0ca4cf1e3c3e2cca897aba0f43d3a5aa22ee2d4e
Ubuntu <ubuntu@ip-10-63-179-124.ap-southeast-2.compute.internal>
2026-09-24 22:23:11 +0000

    Add Jest unit testing for CI pipeline

---
+++ tests/app.test.js
@@ -0,0 +1,11 @@
+describe("Node Application Test", () => {
+
+    test("Application should run correctly", () => {
+
+        const status = true;
+
+        expect(status).toBe(true);
+
+    });
+
+});
